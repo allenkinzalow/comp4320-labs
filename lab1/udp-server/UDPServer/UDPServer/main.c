bc97e38d5d77ed3e2c22f726374eb53516e138e3
Harry Summers <hsummers96@gmail.com>
2018-09-02 15:09:52 -0500

    XCode projects initialized

---
+++ lab1/udp-server/UDPServer/UDPServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  UDPServer
+//
+//  Created by Harry Summers on 9/2/18.
+//  Copyright © 2018 harrysummers. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
